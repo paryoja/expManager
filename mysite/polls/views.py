0c7cef16e789c02973eb8dc9bc22ca9fbfa60ec3
Yoonjae Park <yjpark@kdd.snu.ac.kr>
2016-07-20 14:41:57 +0900

    results

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -9,7 +9,7 @@ def detail(request, question_id):
     return HttpResponse("Your're looking at question %s." % question_id)
 
 
-def result(request, question_id):
+def results(request, question_id):
     response = "You're looking at the results of question %s."
     return HttpResponse(response % question_id)
 

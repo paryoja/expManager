414ea91ae8f45d0cd83fabfc1b1a4ef3ec4b7f04
Yoonjae Park <yjpark@kdd.snu.ac.kr>
2016-07-21 11:27:34 +0900

    organize imports

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,7 +1,6 @@
-from django.http import HttpResponse, Http404, HttpResponseRedirect
-from django.template import loader
-from django.shortcuts import render, get_object_or_404
 from django.core.urlresolvers import reverse
+from django.http import HttpResponseRedirect
+from django.shortcuts import render, get_object_or_404
 from django.views import generic
 
 from .models import Question, Choice
@@ -30,7 +29,8 @@ def vote(request, question_id):
     try:
         selected_choice = question.choice_set.get(pk=request.POST['choice'])
     except (KeyError, Choice.DoesNotExist):
-        return render(request, 'polls/detail.html', { 'question': question, 'error_message': "You didn't select a choice.", })
+        return render(request, 'polls/detail.html',
+                      {'question': question, 'error_message': "You didn't select a choice.",})
     else:
         selected_choice.votes += 1
         selected_choice.save()

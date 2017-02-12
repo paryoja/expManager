6d71106f9994c1b69967dbd055a8160914abaeaf
yjpark on mangosteen <yjpark@kdd.snu.ac.kr>
2017-02-12 17:07:37 +0900

    dataset

--- mysite/projectManager/views.py
+++ mysite/projectManager/views.py
@@ -72,7 +72,8 @@ class ExpView(generic.DetailView):
     def get_context_data(self, **kwargs):
         context = super(ExpView, self).get_context_data(**kwargs)
         context['exp_list'] = context['project'].expitem_set.order_by('-exp_date')[:10]
-        context['dataset_list'] = context['project'].dataset_set.order_by('size')
+        context['dataset_list'] = context['project'].dataset_set.order_by('name')
+        #print( type( context[ 'project' ].dataset_set.order_by('name') ) )
 
         return context
 
